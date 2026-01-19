42507ec0157a9773e744a99247a9ab8bee7cbb33
Ayush Basak <basakayush942@gmail.com>
2026-01-19 16:05:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "*", // Allow all origins for deployed frontend
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
@@ -43,3 +43,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
